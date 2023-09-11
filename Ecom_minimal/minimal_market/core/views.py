71c3a4aa79acb7c8f1139627e4af1f17f51126b1
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-09-11 16:32:28 +0530

    Added signup page UI

--- Ecom_minimal/minimal_market/core/views.py
+++ Ecom_minimal/minimal_market/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from .forms import SignupForm
 
 # Create your views here.
 def index(request):
@@ -12,3 +13,10 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
