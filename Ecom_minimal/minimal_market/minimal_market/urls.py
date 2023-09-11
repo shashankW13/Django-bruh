368200f00c365e37ecda8b0bceae3ba32e076ef0
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-09-11 13:34:59 +0530

    Added front page, product page, related items feature

--- Ecom_minimal/minimal_market/minimal_market/urls.py
+++ Ecom_minimal/minimal_market/minimal_market/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('contact/', contact, name='contact')
+    path('contact/', contact, name='contact'),
+    path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
