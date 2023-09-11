de7506611e78db9da298e85f0a1639f29f73d5cb
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-09-11 13:45:09 +0530

    Added Core App urls

--- Ecom_minimal/minimal_market/minimal_market/urls.py
+++ Ecom_minimal/minimal_market/minimal_market/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
