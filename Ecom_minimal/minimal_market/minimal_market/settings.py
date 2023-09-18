588bc9ba900ee10f7d5e811db8123e9a954a3cc9
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-09-19 02:09:08 +0530

    Changed and added few more vercel configs

--- Ecom_minimal/minimal_market/minimal_market/settings.py
+++ Ecom_minimal/minimal_market/minimal_market/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g&)#zghn%sck$h3s#v5y$ixs_t-b1els0_yap4*kbg==&i^!z)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'minimal_market.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
