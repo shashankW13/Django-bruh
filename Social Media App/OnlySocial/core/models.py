80d265e66381c996e8af4a74070c86689b487dc5
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-09-26 16:34:54 +0530

    Created project setup, added static and media settings, added Profile model

---
+++ Social Media App/OnlySocial/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
