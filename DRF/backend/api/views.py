db1fd17bb79d27397bcb28d947c914a24a9c3a48
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-10-18 14:40:52 +0530

    Created a Product model and echoed data from that model

--- DRF/backend/api/views.py
+++ DRF/backend/api/views.py
@@ -1,18 +1,15 @@
 from django.http import JsonResponse
 import json
+from products.models import Product
 
 
 def api_home(request):
-    body = request.body
-    print(request.GET)
-    print(request.POST)
+    model_data= Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
