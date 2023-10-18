5fade6edf86f0c876ed2caa306b0aac6ae6f9f04
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-10-18 13:43:34 +0530

    Echo back GET data

--- DRF/backend/api/views.py
+++ DRF/backend/api/views.py
@@ -1,5 +1,18 @@
 from django.http import JsonResponse
+import json
 
 
 def api_home(request):
-    return JsonResponse({"msg": "Djnago api response"})
+    body = request.body
+    print(request.GET)
+    print(request.POST)
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- DRF/py_client/basic.py
+++ DRF/py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello"})
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello"})
 print(get_response.json())
 # print(get_response.text)
 # print(get_response.status_code)
