ac9cc453bae40ff7f031a83128828564a33b9adc
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-10-19 14:01:45 +0530

    Created and tested Product model serializers

--- DRF/backend/api/views.py
+++ DRF/backend/api/views.py
@@ -1,15 +1,21 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
 from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.serializers import ProductSerializer
 
 
+@api_view(["GET"])
 def api_home(request):
-    model_data= Product.objects.all().order_by("?").first()
+    """DRF API View"""
+    instance= Product.objects.all().order_by("?").last()
     data = {}
     
-    if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, headers={'content-type': 'application/json'})
--- DRF/py_client/basic.py
+++ DRF/py_client/basic.py
@@ -5,6 +5,7 @@ import requests
 endpoint = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello"})
+print(get_response.text)
 print(get_response.json())
-# print(get_response.text)
+# print(get_response.headers)
 # print(get_response.status_code)
