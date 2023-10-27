7f787166393107b48c206c85cfe893d4267b26f1
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-10-27 23:00:18 +0530

    Completed basic setup and added GET views for monthly challenges

---
+++ Monthly Challenges App/monthly_challenges/challenges/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:month>', views.monthly_challenge_by_number, name='month-number'),
+    path('<str:month>', views.monthly_challenge, name='monthly-challenge'),
+]
---
+++ Monthly Challenges App/monthly_challenges/challenges/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+from calendar import month_name
+
+monthly_challenges = {}
+
+challenges = ['eat', 'walk', 'run', 'dance', 
+                      'eat', 'walk', 'run', 'dance', 
+                      'eat', 'walk', 'run', 'dance']
+
+for month_count in range(0, len(month_name) - 1):
+    monthly_challenges[month_name[month_count + 1].lower()] = challenges[month_count]
+
+def index(request):
+    list_items = ''
+    months = list(monthly_challenges.keys())
+
+    return render(request, 
+                    'challenges/index.html',
+                    context={
+                        "months": months,
+                    })
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    try:
+        redirect_month = months[month - 1]
+        redirect_path = reverse('monthly-challenge', args=[month])
+        return render(request, 'challenges/challenge.html')
+    except:
+        return HttpResponseNotFound('Invalid Month')
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month.lower()]
+        response_data = render(request, 'challenges/challenge.html', 
+                                            context={
+                                                'challenge': challenge_text,
+                                                'month': month,
+                                            })
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound('Month not supported')
+    
