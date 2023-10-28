56f65b6b9a5800a6075710bec193d00d5a93f0c3
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-10-28 17:54:25 +0530

    Added and imported CSS styling in django-html

--- Monthly Challenges App/monthly_challenges/challenges/views.py
+++ Monthly Challenges App/monthly_challenges/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from calendar import month_name
@@ -8,10 +8,11 @@ monthly_challenges = {}
 
 challenges = ['eat', 'walk', 'run', 'dance', 
                       'eat', 'walk', 'run', 'dance', 
-                      'eat', 'walk', 'run', 'dance']
+                      'eat', 'walk', 'run', None]
 
 for month_count in range(0, len(month_name) - 1):
     monthly_challenges[month_name[month_count + 1].lower()] = challenges[month_count]
+
 
 def index(request):
     list_items = ''
@@ -23,6 +24,7 @@ def index(request):
                         "months": months,
                     })
 
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     try:
@@ -30,7 +32,8 @@ def monthly_challenge_by_number(request, month):
         redirect_path = reverse('monthly-challenge', args=[month])
         return render(request, 'challenges/challenge.html')
     except:
-        return HttpResponseNotFound('Invalid Month')
+        raise Http404()
+    
 
 def monthly_challenge(request, month):
     try:
@@ -42,5 +45,5 @@ def monthly_challenge(request, month):
                                             })
         return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound('Month not supported')
+        raise Http404()
     
