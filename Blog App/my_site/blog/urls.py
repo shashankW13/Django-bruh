9d2b65ff5726ed90f7fa592a9f0490eb5b3de8bb
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-11-10 15:20:54 +0530

    Added hardcoded blog views, main page, blog detail detail templates

--- Blog App/my_site/blog/urls.py
+++ Blog App/my_site/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts', views.all_posts, name='all-posts')
+    path('posts', views.all_posts, name='all-posts'),
+    path('posts/<slug:slug>', views.post_detail, name='post-detail'),
 ]
--- Blog App/my_site/blog/views.py
+++ Blog App/my_site/blog/views.py
@@ -1,16 +1,111 @@
 from django.shortcuts import render
 from .models import Blog
+from datetime import date
+
+posts = [
+    {
+        'slug': 'hike-in-the-mountains',
+        'image': 'mountains.jpg',
+        'author': 'Shashank',
+        'date': date(2023, 11, 3),
+        'title': 'Mountain Hiking',
+        'excerpt': """There\'s nothing like then views when 
+                    hiking in the mountains! 
+                    And I wasn\'t even prepared for 
+                    what happened whilst I was enjoying the view!""",
+        'content': """
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quisquam, unde corporis ducimus odio quas ipsa? Dicta recusandae iure, 
+                    corporis atque ea harum ratione animi ullam.
+                    Culpa dolores cumque eveniet totam?
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quisquam, unde corporis ducimus odio quas ipsa? Dicta recusandae iure, 
+                    corporis atque ea harum ratione animi ullam.
+                    Culpa dolores cumque eveniet totam?
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quisquam, unde corporis ducimus odio quas ipsa? Dicta recusandae iure, 
+                    corporis atque ea harum ratione animi ullam.
+                    Culpa dolores cumque eveniet totam?
+                    """,
+    },
+    {
+        'slug': 'programming-guy',
+        'image': 'coding.jpg',
+        'author': 'Shashank',
+        'date': date(2023, 11, 3),
+        'title': 'Programming',
+        'excerpt': """There\'s nothing like then views when 
+                    hiking in the mountains! 
+                    And I wasn\'t even prepared for 
+                    what happened whilst I was enjoying the view!""",
+        'content': """
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quisquam, unde corporis ducimus odio quas ipsa? Dicta recusandae iure, 
+                    corporis atque ea harum ratione animi ullam.
+                    Culpa dolores cumque eveniet totam?
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quisquam, unde corporis ducimus odio quas ipsa? Dicta recusandae iure, 
+                    corporis atque ea harum ratione animi ullam.
+                    Culpa dolores cumque eveniet totam?
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quisquam, unde corporis ducimus odio quas ipsa? Dicta recusandae iure, 
+                    corporis atque ea harum ratione animi ullam.
+                    Culpa dolores cumque eveniet totam?
+                    """,
+    },
+    {
+        'slug': 'wood-exploring',
+        'image': 'woods.jpg',
+        'author': 'Shashank',
+        'date': date(2023, 11, 3),
+        'title': 'Woods Exploring',
+        'excerpt': """There\'s nothing like then views when 
+                    hiking in the mountains! 
+                    And I wasn\'t even prepared for 
+                    what happened whilst I was enjoying the view!""",
+        'content': """
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quisquam, unde corporis ducimus odio quas ipsa? Dicta recusandae iure, 
+                    corporis atque ea harum ratione animi ullam.
+                    Culpa dolores cumque eveniet totam?
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quisquam, unde corporis ducimus odio quas ipsa? Dicta recusandae iure, 
+                    corporis atque ea harum ratione animi ullam.
+                    Culpa dolores cumque eveniet totam?
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quisquam, unde corporis ducimus odio quas ipsa? Dicta recusandae iure, 
+                    corporis atque ea harum ratione animi ullam.
+                    Culpa dolores cumque eveniet totam?
+                    """,
+    }
+]
+
+def get_date(post):
+    return post['date']
 
 def index(request):
-    blog = Blog.objects.all()
+    sorted_posts = sorted(posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
     return render(request, 'blog/index.html', 
                   context={
-                      'blogs': blog,
+                      'posts': latest_posts,
                   })
 
 def all_posts(request):
-    blog = Blog.objects.all()
-    return render(request, 'blog/posts.html', 
+    return render(request, 'blog/all-posts.html', 
                   context={
-                      'blogs': blog,
+                      'all_posts': posts,
+                  })
+
+def post_detail(request, slug):
+    identified_post = next(post for post in posts if post['slug'] == slug)
+    return render(request, 'blog/post-detail.html',
+                  context={
+                      'post': identified_post,
                   })
