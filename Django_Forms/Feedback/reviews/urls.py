cc480f672248d39bc2da7323fa74746a36c90217
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-11-20 15:37:08 +0530

    Added different views - ListView, DetailView, CreateView, FormView

--- Django_Forms/Feedback/reviews/urls.py
+++ Django_Forms/Feedback/reviews/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.ReviewView.as_view(), name='review'),
     path('thank-you', views.ThankYouView.as_view(), name='thank-you'),
     path('reviews', views.ReviewsListView.as_view(), name='reviews'),
-    path('reviews/<int:id>', views.SingleReviewView.as_view(), name='single-review'),
+    path('reviews/<int:pk>', views.SingleReviewView.as_view(), name='single-review'),
 ]
--- Django_Forms/Feedback/reviews/views.py
+++ Django_Forms/Feedback/reviews/views.py
@@ -1,31 +1,20 @@
 from typing import Any
+from django.db import models
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-
-        return render(request, 'reviews/review.html', 
-                  context={
-                      'form': form
-                  })
-    
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/thank-you')
-        
-        return render(request, 'reviews/review.html', 
-                  context={
-                      'form': form
-                  })
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/review.html'
+    success_url = '/thank-you'
 
 class ThankYouView(TemplateView):
     template_name = 'reviews/thank_you.html'
@@ -35,22 +24,12 @@ class ThankYouView(TemplateView):
         context['message'] = 'This works'
         return context
     
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
     template_name = 'reviews/review_list.html'
+    model = Review
+    context_object_name = 'reviews'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context['reviews'] = reviews
-        return context
     
-
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = 'reviews/single_review.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs['id']
-        selected_review = Review.objects.get(pk=review_id)
-        context['review'] = selected_review
-        return context
+    model = Review
