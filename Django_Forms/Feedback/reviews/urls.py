2c49f0ebe61ea4445591289ef1a3a4f4ca66c701
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-11-21 14:20:44 +0530

    Added favorite review functionality using sessions

--- Django_Forms/Feedback/reviews/urls.py
+++ Django_Forms/Feedback/reviews/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.ReviewView.as_view(), name='review'),
     path('thank-you', views.ThankYouView.as_view(), name='thank-you'),
     path('reviews', views.ReviewsListView.as_view(), name='reviews'),
+    path('reviews/favorite', views.AddFavorite.as_view(), name='add-favorite'),
     path('reviews/<int:pk>', views.SingleReviewView.as_view(), name='single-review'),
 ]
